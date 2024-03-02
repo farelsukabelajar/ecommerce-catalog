cf37417baaf42e884807485be4c44fc72224bd8d
Key Firdausi Alfarel <144311812+farelsukabelajar@users.noreply.github.com>
2024-03-02 23:42:09 +0700

    Add files via upload

---
+++ ecommerce-catalog-master/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <ProductDisplay/>
+  </div>
+</template>
+
+<script>
+import ProductDisplay from './components/Product.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ProductDisplay
+  }
+}
+</script>
+
+<style>
+/* css */
+body {
+  margin: 0;
+}
+</style>
